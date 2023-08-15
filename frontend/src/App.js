b6328b2a8a35e17b71d042bd7a0bfb2f06ba0c63
Eszter Erdélyi <esztergyerdelyi@gmail.com>
2023-08-15 18:46:13 +0200

    take router into App.js form Index.js, add DateContext to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
+import React, {createContext, useState} from "react";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Adder from "./Pages/Adder";
+import Main from "./Pages/Main";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main/>
+    },
+    {
+        path: "/add",
+        element: <Adder/>
+    }
+])
+
+export const DateContext = createContext({date: null, setDate: () => {}})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>Cost Calendar</div>
-      </header>
-    </div>
-  );
+    const [date, setDate] = useState();
+    return (
+        <DateContext.Provider value={{date, setDate}}>
+            <div>
+                <RouterProvider router={router}/>
+            </div>
+        </DateContext.Provider>
+    );
 }
 
 export default App;
+
