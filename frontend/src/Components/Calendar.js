e740b766ed204bde8ec6532006c537baa0afd59a
erdelyieszter <esztergyerdelyi@gmail.com>
2023-10-05 11:29:43 +0200

    change Calendar style

--- frontend/src/Components/Calendar.js
+++ frontend/src/Components/Calendar.js
@@ -3,19 +3,22 @@ import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
 import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
 import {DateCalendar} from '@mui/x-date-pickers/DateCalendar';
 import {useNavigate} from "react-router-dom";
+import './Calendar.css';
 
 function BasicDateCalendar() {
     const navigate = useNavigate();
 
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateCalendar
-                sx={{
-                    width: "400px",
-                    backgroundColor: "lightblue",
-                    opacity: "0.8"
-                }
-                } onChange={(value) => {
+            <DateCalendar className={"MuiCalendarPicker-root"}
+                          sx={{
+                              width: "650px",
+                              minHeight: "480px",
+                              borderRadius: 8,
+                              backgroundColor: "lightblue",
+                              opacity: "0.8",
+                          }
+                          } onChange={(value) => {
                 const newDate = new Date(value.$y, value.$M, value.$D).toLocaleDateString('sv-SE')
                 navigate("/add/" + newDate);
             }}/>
